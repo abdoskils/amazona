49c07695e878c826c869f66652cf393e78a94fd7
abdoskils <79817515+abdoskils@users.noreply.github.com>
2021-04-11 03:03:42 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://amazona:abdoskils1234@@amazona.r74mf.mongodb.net/amazona?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://amazona:abdoskils1234@@amazona.r74mf.mongodb.net/amazona?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
